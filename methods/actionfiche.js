f9e73c502ad89216b52c3aa0cca22882c4a3881f
salim ettayeb <selim.ettayeb@gmail.com>
2021-05-13 02:02:27 +0100

    esd

--- methods/actionfiche.js
+++ methods/actionfiche.js
@@ -5,11 +5,7 @@ var config = require('../config/dbconfig')
 var functions = {
     addNewFiche: function (req, res) {
         console.log(req.body)
-        if ((!req.body.filenumber) || 
-            (!req.body.firstname) || (!req.body.lastname) || (!req.body.dateofbirth) ||
-            (!req.body.age) || (!req.body.profession) || (!req.body.valueChoose) ||
-             (!req.body.childChosed) || (!req.body.phonenumber) (!req.body.email) ||
-         (!req.body.address) || (!req.body.cityChosed)) {
+        if ((!req.body.filenumber) || (!req.body.firstname) || (!req.body.lastname) || (!req.body.dateofbirth) ||(!req.body.age) || (!req.body.profession) || (!req.body.valueChoose) || (!req.body.childChosed) || (!req.body.phonenumber) || (!req.body.email) || (!req.body.address) || (!req.body.cityChosed)) {
             res.json({success: false, msg: 'Enter all fields'})
         }
         else {
@@ -17,7 +13,7 @@ var functions = {
                 filenumber: req.body.filenumber,
                 firstname: req.body.firstname,
                 lastname: req.body.lastname,
-                dateOfBirth: req.body.dateOfBirth,
+                dateofbirth: req.body.dateofbirth,
                 age: req.body.age,
                 profession: req.body.profession,
                 valueChoose: req.body.valueChoose,
