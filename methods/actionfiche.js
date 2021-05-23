15f6c0809e1e0d731bc448f026f065671158a228
salim ettayeb <selim.ettayeb@gmail.com>
2021-05-23 04:25:52 +0100

    test33

--- methods/actionfiche.js
+++ methods/actionfiche.js
@@ -5,6 +5,8 @@ var config = require('../config/dbconfig')
 var functions = {
     addNewFiche: function (req, res) {
         console.log(req.body)
+        console.log("ddddddddddddd")
+
         if ((!req.body.doctorId) || (!req.body.groupValue) || (!req.body.filenumber) || (!req.body.firstname) || (!req.body.lastname) || (!req.body.dateofbirth) ||(!req.body.age) || (!req.body.profession) || (!req.body.valueChoose) || (!req.body.childChosed) || (!req.body.phonenumber) || (!req.body.email) || (!req.body.address) || (!req.body.cityChosed)) {
             res.json({success: false, msg: 'Enter all fields'})
         }
