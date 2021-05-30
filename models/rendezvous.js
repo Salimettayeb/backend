a46a97d5bf4540b4e656f9117e511d8fd67b5358
salim ettayeb <selim.ettayeb@gmail.com>
2021-05-31 00:02:35 +0100

    dfdfd

--- models/rendezvous.js
+++ models/rendezvous.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose')
 var Schema = mongoose.Schema;
 var bcrypt = require('bcrypt')
 var rendezvousSchema = new Schema({
-    doctorId: {
+    userId: {
         type: Schema.Types.ObjectId,
         ref:"User",
         required: true
